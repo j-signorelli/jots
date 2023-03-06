f419cbe7707e6cd0074cbfaa596dfd3563e03a0d
jmsig26 <jms26@illinois.edu>
2023-03-06 16:48:00 -0600

    Setup jots to finally be coded

---
+++ build/CMakeFiles/jots.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/work2/08335/jms26/frontera/jots/jots/src/jots.cpp" "CMakeFiles/jots.dir/jots/src/jots.cpp.o" "gcc" "CMakeFiles/jots.dir/jots/src/jots.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
