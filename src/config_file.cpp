bf7a6be5d4c446a682fbfdec494c5ffde2f648e6
Joseph Signorelli <jms26@illinois.edu>
2023-06-16 16:21:56 -0500

    Specific_Heat_Cp --> Specific_Heat_C

--- src/config_file.cpp
+++ src/config_file.cpp
@@ -50,7 +50,7 @@ void Config::ReadMatProps()
 {
     // Read MaterialProperties
     density = property_tree.get("MaterialProperties.Density", 1.0);
-    Cp = property_tree.get("MaterialProperties.Specific_Heat_Cp", 1000.0);
+    Cp = property_tree.get("MaterialProperties.Specific_Heat_C", 1000.0);
     SetInputStringVector(property_tree.get("MaterialProperties.Thermal_Conductivity_Model", "Uniform, 100"), conductivity_info);
 }
 
