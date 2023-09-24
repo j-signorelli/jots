de586d87e2238d54ef0497acc312312c401ba4ac
Joseph Signorelli <jms26@illinois.edu>
2023-09-24 17:01:18 -0500

    More cleanups

--- tests/Reinert_B2.cpp
+++ tests/Reinert_B2.cpp
@@ -2,11 +2,6 @@
 
 #include "test_helper.hpp"
 
-using namespace std;
-using namespace mfem;
-
-double Reinert_B2_Analytical(const Vector& x, double time);
-
 int main(int argc, char *argv[])
 {  
     return Reinert_Test(2, Reinert_B2_Analytical);
