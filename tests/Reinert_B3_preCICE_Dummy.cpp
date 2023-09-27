6ec42aacb81e24d3fd4063076fe4c1c4b076307f
Joseph Signorelli <jms26@illinois.edu>
2023-09-27 12:31:52 -0500

    Added Reinert_B3_preCICE test

--- tests/Reinert_B3_preCICE_Dummy.cpp
+++
@@ -1,8 +0,0 @@
-// This is a C++ version of Reinert_B3_preCICE dummy
-
-#include "test_helper.hpp"
-
-int main(int argc, char **argv)
-{
-    return Reinert_preCICE_Dummy(3, "Temperature", "Heat-Flux", 0.0, 7.5e5, Reinert_B3_Analytical);
-}
