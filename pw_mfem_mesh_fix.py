af4b9c141af771cc161515fc45c7bf3279da2657
Joseph Signorelli <jms26@illinois.edu>
2023-10-26 17:59:32 -0500

    Fixed MFEM-fix script to close #9, cleaned up script and added permissions in CMakeLists

--- pw_mfem_mesh_fix.py
+++ pw_mfem_mesh_fix.py
@@ -4,9 +4,9 @@ Script to fix PW-exported MFEM mesh files
 """
 
 from optparse import OptionParser
+import os
 
-def decrementIndices(line, startingIndex):
-    line_arr = line.split()
+def decrementIndices(line_arr, startingIndex):
 
     for i in range(startingIndex, len(line_arr)):
         line_arr[i] = str(int(line_arr[i]) - 1)
@@ -22,11 +22,12 @@ def main():
 
     (options, args) = parser.parse_args()
 
-
+    max_bdr_attr = 0
     reading_elements = False
     reading_boundaries = False
 
-    w = open(str("fixed_" + options.filename), "w")
+    fixed_filename = str("fixed_" + options.filename)
+    w = open(fixed_filename, "w")
     f = open(options.filename, "r")
 
     for i, line in enumerate(f):
@@ -34,26 +35,32 @@ def main():
         if i == 2:
             w.write("# PW Export Fix Script Applied To This File\n")
 
-        if reading_elements:
+        if reading_elements or reading_boundaries:
             # Update vertex indices
 
-            # If have reached blank line, continue:
+            # If have reached blank line, continue
             if line == "" or line == "\n":
                 reading_elements = False
-                continue
-            
-            w.write(decrementIndices(line,2) + "\n")
-            
-        elif reading_boundaries:
-            # If have reached blank line, continue:
-            if line == "" or line == "\n":
                 reading_boundaries = False
+                w.write(line)
                 continue
             
-            if "vertices" in line:
-                breakpoint()
 
-            w.write(decrementIndices(line,2) + "\n")
+            # Skip number of elements/bdrs line
+            line_arr = line.split()
+            if len(line_arr) == 1:
+                w.write(line)
+                continue
+            
+            if reading_boundaries:
+                # Get max bdr attribute
+                bdr_attr = int(line_arr[0])
+                if bdr_attr > max_bdr_attr:
+                    max_bdr_attr = bdr_attr
+
+            # Decrement indices
+            w.write(decrementIndices(line_arr,2) + "\n")
+            
         else:
 
             if "elements" in line:
@@ -66,6 +73,10 @@ def main():
 
     w.close()
     f.close()
+
+    # Update all max integer element attributes to be max_bdr_attr + 1
+    # Credit to mfem/mfem#3927
+    os.system("sed -i 's/2147483647/" + str(max_bdr_attr+1) + "/g' " + fixed_filename)
 
 # Can run only from terminal
 if __name__ == '__main__':
