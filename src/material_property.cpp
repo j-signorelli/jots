37a943673720e6a1ded91bdab2702aaa789f2490
Joseph Signorelli <jms26@illinois.edu>
2023-11-07 18:03:32 -0600

    Generalized variable names + cleaned up + prepped for nonlinear solve capability

--- src/material_property.cpp
+++ src/material_property.cpp
@@ -3,11 +3,42 @@
 using namespace std;
 using namespace mfem;
 
+UniformProperty::UniformProperty(const double& in_mp)
+: MaterialProperty(),
+  mp_val(in_mp)
+{
+    coeff = new ConstantCoefficient(mp_val);
+}
+
 string UniformProperty::GetInitString() const
 {
     stringstream sstm;
     sstm << "Uniform --- Value = " << k;
     return sstm.str();
+}
+
+PolynomialProperty::PolynomialProperty(const std::vector<double>& in_poly_coeffs, ParFiniteElementSpace& f) 
+: MaterialProperty(), 
+  poly_coeffs(in_poly_coeffs),
+  mp_gf(&f),
+  u_gf(&f),
+  z(&f)
+{
+    coeff = new GridFunctionCoefficient(mp_gf);
+}
+
+void PolynomialProperty::UpdateCoeff(const mfem::Vector& u_ref)
+{   
+    u_gf.SetFromTrueDofs(u_ref);
+    
+    *mp_gf = 0;
+    for (size_t i = 0; i < poly_coeffs.size(); i++)
+    {
+        z = 1;
+        for (size_t j = 0; j < poly_coeffs.size() - i - 1; j++)
+            z *= u_gf;
+        mp_gf->Add(poly_coeffs[i],z); // Update the GF associated with the coefficient
+    }
 }
 
 string PolynomialProperty::GetInitString() const
@@ -20,43 +51,19 @@ string PolynomialProperty::GetInitString() const
         sstm << poly_coeffs[i];
         exp = poly_coeffs.size() - i - 1;
         if (exp == 1)
-            sstm << "T + ";
+            sstm << "u + ";
         else if (exp != 0)
-            sstm << "T^" << exp << " + ";
+            sstm << "u^" << exp << " + ";
     }
     return sstm.str();
 }
 
-PolynomialProperty::PolynomialProperty(const std::vector<double> in_poly_coeffs, ParFiniteElementSpace& f) 
-: MaterialProperty(), 
-  poly_coeffs(in_poly_coeffs),
-  T_gf(&f),
-  z(&f)
-{
-    k_gf = new ParGridFunction(&f);
-    
-    coeff = new GridFunctionCoefficient(k_gf);
-}
 
-void PolynomialProperty::UpdateCoeff(const mfem::Vector& T_ref)
-{   
-    T_gf.SetFromTrueDofs(T_ref);
-    
-    *k_gf = 0;
-    for (size_t i = 0; i < poly_coeffs.size(); i++)
-    {
-        z = 1;
-        for (size_t j = 0; j < poly_coeffs.size() - i - 1; j++)
-            z *= T_gf;
-        k_gf->Add(poly_coeffs[i],z); // Update the GF associated with the coefficient
-    }
-}
-
-double PolynomialProperty::GetLocalValue(double temp) const
+double PolynomialProperty::GetLocalValue(double u_local) const
 {
     double k = 0;
     for (size_t i = 0; i < poly_coeffs.size(); i++)
-        k += poly_coeffs[i]*pow(temp, poly_coeffs.size() - i - 1);
+        k += poly_coeffs[i]*pow(u_local, poly_coeffs.size() - i - 1);
 
     return k;
 }
--- src/material_property.hpp
+++ src/material_property.hpp
@@ -13,31 +13,30 @@ class MaterialProperty
     protected:
         mfem::Coefficient* coeff;
     public:
+        MaterialProperty() : coeff(nullptr) {};
         mfem::Coefficient& GetCoeffRef() const { return *coeff; }; // To be used only for assigning to linear/bilinear forms or projecting coeff, so declared const
-
-        virtual bool IsConstant() const = 0; // true if dk_du = 0
-        virtual void UpdateCoeff(const mfem::Vector& T_ref) = 0;
-
+        virtual bool IsConstant() const = 0; // true if dcoeff_du = 0
+        virtual void UpdateCoeff(const mfem::Vector& u_ref) = 0;
+    
+        
         virtual std::string GetInitString() const = 0;
-
-        virtual double GetLocalValue(double temp) const = 0;
-
+        virtual double GetLocalValue(double u_local) const = 0;
+        virtual double GetLocalDerivative(double u_local) const = 0; // Used for Newton-Raphson iterations
         virtual ~MaterialProperty() { delete coeff; };
 };
 
 class UniformProperty : public MaterialProperty
 {
     private:
-        const double k;
+        const double mp_val;
     protected:
     public:
-        UniformProperty(const double in_k) : MaterialProperty(), k(in_k) { coeff = new mfem::ConstantCoefficient(k); };
+        UniformProperty(const double& in_mp);
         bool IsConstant() const { return true; }
-        void UpdateCoeff(const mfem::Vector& T_ref) {};
-
+        void UpdateCoeff(const mfem::Vector& u_ref) {};
         std::string GetInitString() const;
         
-        double GetLocalValue(double temp) const { return k; };
+        double GetLocalValue(double u_local) const { return mp_val; };
 };
 
 class PolynomialProperty : public MaterialProperty
@@ -45,16 +44,15 @@ class PolynomialProperty : public MaterialProperty
     private:
     protected:
         const std::vector<double> poly_coeffs;
-        mfem::ParGridFunction* k_gf;
+        mfem::ParGridFunction mp_gf; // PGF for coeff
 
-        mutable mfem::ParGridFunction T_gf;
+        mutable mfem::ParGridFunction u_gf;
         mutable mfem::ParGridFunction z;
     public:
-        PolynomialProperty(const std::vector<double> in_poly_coeffs, mfem::ParFiniteElementSpace& f);
+        PolynomialProperty(const std::vector<double>& in_poly_coeffs, mfem::ParFiniteElementSpace& f);
         bool IsConstant() const { return false; };
-        void UpdateCoeff(const mfem::Vector& T_ref);
+        void UpdateCoeff(const mfem::Vector& u_ref);
 
         std::string GetInitString() const;
-        double GetLocalValue(double temp) const;
-        ~PolynomialProperty() { delete k_gf; };
+        double GetLocalValue(double u_local) const;
 };
