6a755f44c60157dcc4933fc7bc4406e9404567b5
Joseph Signorelli <jms26@illinois.edu>
2024-02-23 16:20:04 -0600

    Progress on revamping output_manager

--- src/output_manager.cpp
+++ src/output_manager.cpp
@@ -3,76 +3,75 @@
 using namespace std;
 using namespace mfem;
 
-OutputManager::OutputManager(const int in_rank, ParFiniteElementSpace& f, const Config& user_input)
-: visit_dc(nullptr),
-  paraview_dc(nullptr),
-  fespace(f),
-  rank(in_rank),
+OutputManager::OutputManager(const int in_rank, const Config &user_input, Mesh &mesh)
+: visit_dc(user_input.GetRestartPrefix(), &mesh),
+  paraview_dc("ParaView", &mesh),
   rank_coeff(in_rank)
 {   
     //------------------------------------------------
     // Set up VisIt outputting (restarts)
-    visit_dc = new VisItDataCollection(user_input.GetRestartPrefix(), fespace.GetParMesh());
-    visit_dc->SetLevelsOfDetail(user_input.GetFEOrder());
-    visit_dc->SetFormat(DataCollection::PARALLEL_FORMAT);
-    visit_dc->SetPrecision(16);
+    visit_dc.SetLevelsOfDetail(user_input.GetFEOrder());
+    visit_dc.SetFormat(DataCollection::PARALLEL_FORMAT);
+    visit_dc.SetPrecision(16);
     #ifdef MFEM_USE_ZLIB
-        visit_dc->SetCompression(true);
+        visit_dc.SetCompression(true);
     #endif 
     //------------------------------------------------
     // Set up ParaView outputting
-    paraview_dc = new ParaViewDataCollection("ParaView", fespace.GetParMesh());
-    paraview_dc->UseRestartMode(user_input.UsingRestart());
-    paraview_dc->SetLevelsOfDetail(user_input.GetFEOrder());
-    paraview_dc->SetDataFormat(VTKFormat::BINARY);
-    paraview_dc->SetHighOrderOutput(true);
+    paraview_dc.UseRestartMode(user_input.UsingRestart());
+    paraview_dc.SetLevelsOfDetail(user_input.GetFEOrder());
+    paraview_dc.SetDataFormat(VTKFormat::BINARY);
+    paraview_dc.SetHighOrderOutput(true);
 
     //------------------------------------------------
     // Instantiate rank output
     // Rank:
-    RegisterCoefficient("Rank", rank_coeff);
+    // TODO: Temporarily removed until behavior for different ParFiniteElementSpace's in same DataCollection
+    //       is understood
+    //RegisterCoefficient("Rank", rank_coeff);
 
 }
 
-void OutputManager::RegisterCoefficient(const string output_name, Coefficient& coeff)
+void OutputManager::RegisterCoefficient(const string output_name, Coefficient& coeff, ParFiniteElementSpace& f )
 {
 
     // Create new CoefficientOutput + PGF for it
-    coeff_output_map[output_name] = new CoefficientOutput(coeff, &fespace);
+    coeff_output_map[output_name] = new CoefficientOutput(coeff,f);
 
     // Project coefficient onto it
-    coeff_output_map[output_name]->pgf->ProjectCoefficient(coeff);
+    coeff_output_map[output_name]->pgf.ProjectCoefficient(coeff);
 
     // Register
-    paraview_dc->RegisterField(output_name, coeff_output_map[output_name]->pgf);
+    paraview_dc.RegisterField(output_name, &(coeff_output_map[output_name]->pgf));
+    visit_dc.RegisterField(output_name, &(coeff_output_map[output_name]->pgf));
 }
 
-void OutputManager::RegisterSolutionVector(const string output_name, const Vector& vec)
+void OutputManager::RegisterSolutionVector(const string output_name, const Vector& vec, ParFiniteElementSpace& f)
 {
     // Create new grid function for it and add Vector reference and GF pair to map
-    vector_output_map[output_name] = new VectorOutput(vec, &fespace);
+    vector_output_map[output_name] = new VectorOutput(vec, f);
 
     // Set PGF from vector
-    vector_output_map[output_name]->pgf->SetFromTrueDofs(vec);
+    vector_output_map[output_name]->pgf.SetFromTrueDofs(vec);
 
     // Register to both ParaView + Restarts
-    paraview_dc->RegisterField(output_name, vector_output_map[output_name]->pgf);
-    visit_dc->RegisterField(output_name, vector_output_map[output_name]->pgf);
+    paraview_dc.RegisterField(output_name, &(vector_output_map[output_name]->pgf));
+    visit_dc.RegisterField(output_name, &(vector_output_map[output_name]->pgf));
 }
 
 void OutputManager::UpdateGridFunctions()
 {
     // Update all coefficient-driven GFs
     for (auto& x : coeff_output_map) // x.second is the value of a given {key,value} pair
-        x.second->pgf->ProjectCoefficient(x.second->coeff_ref);
+        x.second->pgf.ProjectCoefficient(x.second->coeff_ref);
 
     // Update all vector-driven GFs
     for (auto& x : vector_output_map)
-        x.second->pgf->SetFromTrueDofs(x.second->vector_ref);
+        x.second->pgf.SetFromTrueDofs(x.second->vector_ref);
 
 }
 
-const ParGridFunction* OutputManager::GetVectorPGF(string vec_label)
+const ParGridFunction& OutputManager::GetVectorPGF(string vec_label)
 {
     UpdateGridFunctions();
     return vector_output_map[vec_label]->pgf;
@@ -81,23 +80,20 @@ const ParGridFunction* OutputManager::GetVectorPGF(string vec_label)
 void OutputManager::WriteVizOutput(const int it_num, const double time)
 {   
     UpdateGridFunctions();
-    paraview_dc->SetCycle(it_num);
-    paraview_dc->SetTime(time);
-    paraview_dc->Save();
+    paraview_dc.SetCycle(it_num);
+    paraview_dc.SetTime(time);
+    paraview_dc.Save();
 }
 
 void OutputManager::WriteRestartOutput(const int it_num, const double time)
 {
     UpdateGridFunctions();
-    visit_dc->SetCycle(it_num);
-    visit_dc->SetTime(time);
-    visit_dc->Save();
+    visit_dc.SetCycle(it_num);
+    visit_dc.SetTime(time);
+    visit_dc.Save();
 }
-
 OutputManager::~OutputManager()
 {
-    delete visit_dc;
-    delete paraview_dc;
     for (auto& x : coeff_output_map)
         delete x.second;
     for (auto& x : vector_output_map)
--- src/output_manager.hpp
+++ src/output_manager.hpp
@@ -9,31 +9,26 @@
 struct CoefficientOutput
 {
     mfem::Coefficient& coeff_ref; //ProjectCoefficient not const! Cannot have as const
-    mfem::ParGridFunction* pgf;
+    mfem::ParGridFunction pgf;
 
-    CoefficientOutput(mfem::Coefficient& in_coeff, mfem::ParFiniteElementSpace* f) : coeff_ref(in_coeff), pgf(nullptr) { pgf = new mfem::ParGridFunction(f); };
-    ~CoefficientOutput() { delete pgf; };
+    CoefficientOutput(mfem::Coefficient &in_coeff, mfem::ParFiniteElementSpace &f) : coeff_ref(in_coeff), pgf(&f) {};
 };
 
 
 struct VectorOutput
 {
     const mfem::Vector& vector_ref;
-    mfem::ParGridFunction* pgf;
+    mfem::ParGridFunction pgf;
 
-    VectorOutput(const mfem::Vector& in_vec, mfem::ParFiniteElementSpace* f) : vector_ref(in_vec), pgf(nullptr) { pgf = new mfem::ParGridFunction(f); };
-    ~VectorOutput() { delete pgf; };
+    VectorOutput(const mfem::Vector &in_vec, mfem::ParFiniteElementSpace &f) : vector_ref(in_vec), pgf(&f) {};
 };
 
 class OutputManager
 {
     private:
-        mfem::VisItDataCollection* visit_dc;
-        mfem::ParaViewDataCollection* paraview_dc;
-
-        mfem::ParFiniteElementSpace& fespace;
+        mfem::VisItDataCollection visit_dc;
+        mfem::ParaViewDataCollection paraview_dc;
         
-        const int rank;
         mfem::ConstantCoefficient rank_coeff;
 
         std::map<std::string, CoefficientOutput*> coeff_output_map;
@@ -43,15 +38,15 @@ class OutputManager
 
     protected:
     public:
-        OutputManager(const int in_rank, mfem::ParFiniteElementSpace& f, const Config& user_input);
+        OutputManager(const int in_rank, const Config &user_input, mfem::Mesh &mesh);
         
-        void RegisterCoefficient(const std::string output_name, mfem::Coefficient& coeff);
-        void RegisterSolutionVector(const std::string output_name, const mfem::Vector& vec);
+        void RegisterCoefficient(const std::string output_name, mfem::Coefficient& coeff, mfem::ParFiniteElementSpace& f);
+        void RegisterSolutionVector(const std::string output_name, const mfem::Vector& vec, mfem::ParFiniteElementSpace& f);
 
         void WriteVizOutput(const int it_num, const double time);
         void WriteRestartOutput(const int it_num, const double time);
 
-        const mfem::ParGridFunction* GetVectorPGF(std::string vec_label);
+        const mfem::ParGridFunction& GetVectorPGF(std::string vec_label);
 
         ~OutputManager();
 };
